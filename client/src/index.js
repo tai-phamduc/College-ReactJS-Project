a4b4eb88fc1df107aab509b7e65a0746ccbc9fe2
xishanht108 <lathanhsi100804@gmail.com>
2025-04-29 00:38:38 +0700

    Fix Tailwind CSS configuration and styling issues

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
-import './assets/index.css';
+import './main.css';
 
 const queryClient = new QueryClient();
 
