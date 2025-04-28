a415bdc4bd34cdd43abf1763faadd35cf086e3b2
xishanht108 <lathanhsi100804@gmail.com>
2025-04-29 00:41:45 +0700

    Fix Tailwind CSS configuration and unknown at rule errors

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
-import './main.css';
+import './input.css';
 
 const queryClient = new QueryClient();
 
