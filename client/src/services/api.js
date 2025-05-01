cb93de89d313de5556895205612377c4a115e7d0
xishanht108 <lathanhsi100804@gmail.com>
2025-05-01 15:13:21 +0700

    Update API service to use local server during development and install concurrently

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance
 const api = axios.create({
-  baseURL: 'https://movie-ticket-booking-api.vercel.app/api',
+  baseURL: 'http://localhost:5010/api',
   headers: {
     'Content-Type': 'application/json',
   },
